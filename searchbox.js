40d571a8f8910b1721dd5626a8c66f1d044b7a06
17071022 <sinanondul@gmail.com>
2020-08-25 18:18:27 +0300

    yes

--- searchbox.js
+++ searchbox.js
@@ -1,7 +1,16 @@
 var list = [
     {
-        "title":        "New York",
-        "url":          "www.sefamerve.com" 
+        "title":        "PASSAT",
+        "url":          "https://www.dogusoto.com.tr/passat-cb2",
+        "IMAGE":        "https://iasbh.tmgrup.com.tr/fc96d1/0/0/0/0/0/0?u=http://i.tmgrup.com.tr/otohaber/album/2019/02/08/1549611491484.jpg&mw=1024",
+        "Marka":        "VOLKSWAGEN",
+        "Kategori":     "ARABA"
+    },{
+        "title":        "iPhone X",
+        "url":          "https://www.apple.com/shop/refurbished/iphone/iphone-x",
+        "IMAGE":        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-iphoneX-spacegray?wid=572&hei=572&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1548459953882",
+        "Marka":        "LAMBORGHINI",
+        "Kategori":     "TELEFON"
     },{
         "title":        "Gallardo",
         "url":          "https://www.lamborghini.com/",
@@ -9,20 +18,20 @@ var list = [
         "Marka":        "LAMBORGHINI",
         "Kategori":     "ARABA"
     },{
-        "title":        "Netherlands",
-        "url":          "www.sefamerve.com"
+        "title":        "Gallardo",
+        "url":          "https://www.lamborghini.com/",
+        "IMAGE":        "https://upload.wikimedia.org/wikipedia/commons/6/67/Lamborghini_Gallardo_3.jpg",
+        "Marka":        "LAMBORGHINI",
+        "Kategori":     "ARABA"
     },{
-        "title":        "Lanzarote",
-        "url":          "www.sefamerve.com"
-    },{
-        "title":        "Italy",
-        "url":          "www.sefamerve.com"
+        "title":        "Gallardo",
+        "url":          "https://www.lamborghini.com/",
+        "IMAGE":        "https://upload.wikimedia.org/wikipedia/commons/6/67/Lamborghini_Gallardo_3.jpg",
+        "Marka":        "LAMBORGHINI",
+        "Kategori":     "ARABA"
     }
 ];
 
-//var obj = JSON.parse(JSON.stringifytest1);
-
-//////////
 
 const searchBar = document.getElementById("searchBar");
 
@@ -34,17 +43,24 @@ var QSearch = false;
 
 searchBar.addEventListener('click', showFrame());
 
+var CpopUp = '<div class="waw-search-container waw-open" id="waw-search-results"></div>';
+
+
 function showFrame(){
     clicked=true;
 };
 
+popUp = document.getElementById("popUp");
+
+
 var results = [];
 searchBar.addEventListener("keyup", (a)=>{
     setTimeout(function() {
-        console.log("checking");
-        if( a.target.value.length >= 3){  
+        stayUp = ( a.target.value.length >= 3);
+        if( stayUp){  
             console.log("Search");
-
+            popUp.innerHTML = CpopUp;
+            sidebar.innerHTML = Cside;
             
             var searchField = "title";
             var searchVal = a.target.value;
@@ -52,14 +68,7 @@ searchBar.addEventListener("keyup", (a)=>{
                 if (list[i][searchField].toLowerCase() == searchVal.toLowerCase()) {
                     results.push(list[i]);
                     console.log(list[i]);
-
-                    //populate
                     newEl = document.getElementById("pr");
-                    //newEl = 
-
-
-
-
             }
         }
 
---
+++ sidebar.js
@@ -0,0 +1,15 @@
+var sidebar = document.getElementById("sidebar")
+
+var Cside =  '<div class="waw-search-sidebar"></div>' +
+ '  <h3 class="waw-category-title">Kategoriler</h3><hr>'+
+    '<li><ul> </ul></li>'+
+     '<h3 class="waw-category-title">Markalar</h3><hr>';
+
+
+
+/* 
+list.forEach(element => {
+    <div class="waw-search-sidebar">
+    <h3 class="waw-category-title">Kategoriler</h3>
+    </div> 
+});*/
