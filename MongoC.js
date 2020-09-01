2aff2ed9d5eeb0d8b75ee7681849d30964b13d93
17071022 <sinanondul@gmail.com>
2020-09-01 17:48:03 +0300

    mong

--- MongoC.js
+++ MongoC.js
@@ -6,11 +6,12 @@ async function main(){
    */
   const uri = "mongodb+srv://suatkuran:EPIKtetos80@cluster0.t1kyp.mongodb.net/scraper?retryWrites=true&w=majority";
 
-  const client = new MongoClient(uri);
+  const client = new MongoClient(uri, { useUnifiedTopology: true } );
 
   try {
       // Connect to the MongoDB cluster
       await client.connect();
+      console.log("cncted")
 
       // Make the appropriate DB calls
       await  listDatabases(client);
@@ -30,3 +31,16 @@ async function listDatabases(client){
   console.log("Databases:");
   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
+let nameOfListing = "Tanzimat Edebiyatı 2"; 
+async function findOneListingByName(client, nameOfListing) {
+  result = await client.db("scraper").collection("collection1")
+                      .findOne({ title: nameOfListing });
+
+  if (result) {
+      console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
+      console.log(result);
+  } else {
+      console.log(`No listings found with the name '${nameOfListing}'`);
+  }
+}
